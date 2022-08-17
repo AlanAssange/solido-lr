6352888294cb18a5a62b04b51411941ef10b5958
AlanAssange <alanjc27@gmail.com>
2022-08-17 16:32:18 -0300

    Últimos cambios responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ export const Home = () => {
           </nav>
           <div className="contents" >
           <img className="log_landing" src={solidocol}/>
-          <p className='log_p'>Sólido: Connecting Solutions | Home</p>
+          <p className='log_p'>Solido: Connecting Solutions | Home</p>
           <Link to="/solido-lr/home"><p className="content_button" >WEB</p></Link>
           <a href="https://soporte.solidcloud.com.ar/"><p className="content_button" >SISTEMA</p></a>
 
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -54,7 +54,7 @@ export const Testimonials = () => {
 
                 </div>
                 <div className="image">
-                    <img src="https://images.pexels.com/photos/756484/pexels-photo-756484.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+                    <img src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
                 </div>
             </div>
             </div>
